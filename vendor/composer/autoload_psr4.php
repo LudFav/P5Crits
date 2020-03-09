71a13d0c23248c15c34fad3d07f2d00981ed275c
LudovicFavory <lu.favory@gmail.com>
2020-03-09 09:03:17 +0100

    Update autoload_psr4.php

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Crits\\' => array($baseDir . '/Portal'),
+    'Crits\\' => array($baseDir . '/portal'),
 );

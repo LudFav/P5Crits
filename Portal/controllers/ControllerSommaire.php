7885abe436fd4eeac7c9015f85e1f196c509092e
LudovicFavory <lu.favory@gmail.com>
2020-03-09 15:48:13 +0100

    Update ControllerSommaire.php

--- Portal/controllers/ControllerSommaire.php
+++ Portal/controllers/ControllerSommaire.php
@@ -1,6 +1,6 @@
 <?php
 namespace CritsPortal\controllers;
-use CritsPortal\views\View;
+
 
 class ControllerSommaire{
   private $_view;
@@ -10,7 +10,7 @@ class ControllerSommaire{
     if (isset($url) && count($url) > 1) {
       throw new \Exception("Page introuvable", 1);
     } else {
-      $this->_view = new View('Sommaire');
+      $this->_view = new \CritsPortal\views\View('Sommaire');
       $this->_view->generate('Sommaire', array());
     } 
   } 

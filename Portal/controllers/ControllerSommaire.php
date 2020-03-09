dd3bfef66c0202ef6bbe92df6f27a364755f67ab
LudovicFavory <lu.favory@gmail.com>
2020-03-09 16:45:52 +0100

    Update ControllerSommaire.php

--- Portal/controllers/ControllerSommaire.php
+++ Portal/controllers/ControllerSommaire.php
@@ -1,7 +1,7 @@
 <?php
 namespace CritsPortal\controllers;
-use CritsPortal\controllers\ajaxClientPhp\ajaxClientBillet;
-
+use CritsPortal\controllers\ajaxClientPhp;
+use CritsPortal\views\View;
 
 class ControllerSommaire{
   private $_view;
@@ -11,7 +11,7 @@ class ControllerSommaire{
     if (isset($url) && count($url) > 1) {
       throw new \Exception("Page introuvable", 1);
     } else {
-      $this->_view = new \CritsPortal\views\View('Sommaire');
+      $this->_view = new View('Sommaire');
       $this->_view->generate('Sommaire', array());
     } 
   } 

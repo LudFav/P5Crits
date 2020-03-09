0c9e129be537157342c7cb9a25ec94a8e9722183
LudovicFavory <lu.favory@gmail.com>
2020-03-09 16:45:44 +0100

    Update ControllerAdmin.php

--- Portal/controllers/ControllerAdmin.php
+++ Portal/controllers/ControllerAdmin.php
@@ -1,11 +1,12 @@
 <?php
+namespace CritsPortal\controllers;
 
 class ControllerAdmin{
    
     private $_view;
     public function __construct(){
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
-        $this->_view = new CritsPortal\views\View('Admin'); 
+        $this->_view = new \CritsPortal\views\View('Admin'); 
         $this->_view->generate('Administration', array());  
     } else {
       header('Location:accueil');

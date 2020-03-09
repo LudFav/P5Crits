30aa1a3779c2c9a6ffa831aed9b208e399bc2ade
LudovicFavory <lu.favory@gmail.com>
2020-03-09 10:16:53 +0100

    Update ControllerSommaire.php

--- Portal/controllers/ControllerSommaire.php
+++ Portal/controllers/ControllerSommaire.php
@@ -1,11 +1,12 @@
 <?php
-use Crits\views\View;
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/controllers/ajaxClientPhp/ajaxClientBillet.php';
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/controllers/ajaxClientPhp/ajaxClientCom.php';
+namespace Crits\portal\controllers;
+use Crits\portal\views\View;
 
 class ControllerSommaire{
   private $_view;
   public function __construct(){
+    require_once('portal/controllers/Router.php');
+
     if (isset($url) && count($url) > 1) {
       throw new \Exception("Page introuvable", 1);
     } else {

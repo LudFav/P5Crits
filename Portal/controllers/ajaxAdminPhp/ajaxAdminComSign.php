9600ec0318adfa6ce670d4ef60d8c9466a36e955
LudovicFavory <lu.favory@gmail.com>
2020-03-09 15:48:28 +0100

    Update ajaxAdminComSign.php

--- Portal/controllers/ajaxAdminPhp/ajaxAdminComSign.php
+++ Portal/controllers/ajaxAdminPhp/ajaxAdminComSign.php
@@ -1,9 +1,8 @@
 <?php
 namespace CritsPortal\controllers\ajaxAdminPhp;
-use CritsPortal\models\CommentManager;
 
 $_commentManager;
-$_commentManager = new CommentManager;
+$_commentManager = new \CritsPortal\models\CommentManager;
 
 $entiteParPage = 4;
 $nbreEntitesParPage=$entiteParPage;

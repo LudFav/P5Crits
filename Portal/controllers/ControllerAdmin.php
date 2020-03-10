93a461f3f1b7c8e58f5acb160e95b7a8be725d56
LudovicFavory <lu.favory@gmail.com>
2020-03-10 10:01:58 +0100

    Update ControllerAdmin.php

--- Portal/controllers/ControllerAdmin.php
+++ Portal/controllers/ControllerAdmin.php
@@ -1,12 +1,16 @@
 <?php
-namespace CritsPortal\controllers;
+require_once 'controllers/ajaxAdminPhp/ajaxAdminBillet.php';
+require_once 'controllers/ajaxAdminPhp/ajaxAdminComSign.php';
+require_once 'controllers/ajaxAdminPhp/ajaxAdminComMod.php';
+
+require_once 'views/View.php';
 
 class ControllerAdmin{
-   
+    private $_billetManager;
     private $_view;
     public function __construct(){
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
-        $this->_view = new \CritsPortal\views\View('Admin'); 
+        $this->_view = new View('Admin'); 
         $this->_view->generate('Administration', array());  
     } else {
       header('Location:accueil');

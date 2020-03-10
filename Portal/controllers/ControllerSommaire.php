821469ad349120e0dd8b445529e02580ca2b0b81
LudovicFavory <lu.favory@gmail.com>
2020-03-10 09:58:41 +0100

    Update ControllerSommaire.php

--- Portal/controllers/ControllerSommaire.php
+++ Portal/controllers/ControllerSommaire.php
@@ -8,7 +8,7 @@ class ControllerSommaire{
   private $_view;
   public function __construct(){
     require_once('portal/controllers/Router.php');
-
+    require_once('portal/controllers/ajaxClientPhp/ajaxClientBillet.php');
     if (isset($url) && count($url) > 1) {
       throw new \Exception("Page introuvable", 1);
     } else {

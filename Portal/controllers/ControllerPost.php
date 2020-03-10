d07e9641a2e97cd102412761ee0b5a3f355c1598
LudovicFavory <lu.favory@gmail.com>
2020-03-10 15:43:22 +0100

    Update ControllerPost.php

--- Portal/controllers/ControllerPost.php
+++ Portal/controllers/ControllerPost.php
@@ -1,12 +1,14 @@
 <?php
-require_once 'views/View.php';
-require_once 'controllers/ajaxClientPhp/ajaxClientCom.php';
+namespace CritsPortal\controllers;
+
+require_once $_SERVER["DOCUMENT_ROOT"]. '/P5Crits/portal/controllers/ajaxClientPhp/ajaxClientCom.php';
 
 class ControllerPost {
   private $_billetManager;
   private $_view;
 
   public function __construct(){
+    require_once('portal/controllers/Router.php');
     if (isset($url) && count($url) < 1) {
       throw new \Exception("Page Introuvable");
     } else {
@@ -21,7 +23,7 @@ class ControllerPost {
       /**
      * Billets a montrer
      */
-      $this->_billetManager = new BilletManager;
+      $this->_billetManager = new \CritsPortal\models\BilletManager;
       $billet = $this->_billetManager->getBillet($_GET['id']);
     
     //VUE
@@ -31,10 +33,10 @@ class ControllerPost {
    
     if($billet == false){
       $errorMsg = 'Billet introuvable';
-      $this->_view = new View('Error');
+      $this->_view = new \CritsPortal\views\View('Error');
       $this->_view->generate('Erreur', array('errorMsg' => $errorMsg));
     } else{
-      $this->_view = new View('SinglePost');
+      $this->_view = new \CritsPortal\views\View('SinglePost');
       $this->_view->generate('Billet', array('billet' => $billet));
     }
     

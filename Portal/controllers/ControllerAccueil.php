918525ebc928438fe3c31bea81bc4c274a42845a
LudovicFavory <lu.favory@gmail.com>
2020-03-10 15:43:14 +0100

    Update ControllerAccueil.php

--- Portal/controllers/ControllerAccueil.php
+++ Portal/controllers/ControllerAccueil.php
@@ -1,13 +1,14 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P4Blog/views/View.php';
+namespace CritsPortal\controllers;
 
 class ControllerAccueil{
   private $_view;
   public function __construct(){
+    require_once('portal/controllers/Router.php');
     if (isset($url) && count($url) > 1) {
       throw new \Exception("Page introuvable", 1);
     } else {
-      $this->_view = new View('Accueil');
+      $this->_view = new \CritsPortal\views\View('Accueil');
       $this->_view->generate("Billet simple pour l'Alaska", array()); 
     } 
   }

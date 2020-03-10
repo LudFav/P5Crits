da015eec63080c8851bfba1d827d16e8b7170219
LudovicFavory <lu.favory@gmail.com>
2020-03-10 10:02:06 +0100

    Update ControllerUpdate.php

--- Portal/controllers/ControllerUpdate.php
+++ Portal/controllers/ControllerUpdate.php
@@ -1,14 +1,12 @@
 <?php
-namespace CritsPortal\controllers;
 
-use CritsPortal\views\View;
-use CritsPortal\models\BilletManager;
+require_once 'views/View.php';
+
 class ControllerUpdate {
   private $_billetManager;
   private $_view;
 
   public function __construct(){
-    require_once('portal/controllers/Router.php');
     if (isset($url) && count($url) < 1) {
         throw new \Exception("Page Introuvable");
     } else {

3580ba2e1ab5a7ace59b7fd4e7390758edc9617f
LudovicFavory <lu.favory@gmail.com>
2020-03-10 16:09:38 +0100

    Update ControllerAdmin.php

--- Portal/controllers/ControllerAdmin.php
+++ Portal/controllers/ControllerAdmin.php
@@ -1,15 +1,16 @@
 <?php
 namespace CritsPortal\controllers;
 use CritsPortal\views\View;
-
+require_once($_SERVER['DOCUMENT_ROOT']. '/P5Crits/vendor/autoload.php');
 require_once('portal/controllers/ajaxAdminPhp/ajaxAdminBillet.php');
 require_once('portal/controllers/ajaxAdminPhp/ajaxAdminComSign.php');
 require_once('portal/controllers/ajaxAdminPhp/ajaxAdminComMod.php');
-require_once($_SERVER['DOCUMENT_ROOT']. '/P5Crits/vendor/autoload.php');
+
 
 class ControllerAdmin{
     private $_view;
     public function __construct(){
+      var_dump($_SESSION);
       if(isset($_SESSION['admin']) && !empty($_SESSION['admin'])){
         echo 'Ca marche';
         $this->_view = new View('Admin'); 

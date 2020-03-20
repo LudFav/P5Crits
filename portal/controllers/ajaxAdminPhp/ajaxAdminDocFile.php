734a05dacb2a192e9bfeee7ab7b9a0fe9ea3553e
LudFav <45117462+LudFav@users.noreply.github.com>
2020-03-20 10:02:21 +0100

    Update ajaxAdminDocFile.php

--- portal/controllers/ajaxAdminPhp/ajaxAdminDocFile.php
+++ portal/controllers/ajaxAdminPhp/ajaxAdminDocFile.php
@@ -1,6 +1,6 @@
 <?php
 use CritsPortal\models\FileManager;
-require_once($_SERVER["DOCUMENT_ROOT"]. '/P5Crits/portal/models/CommentManager.php');
+require_once($_SERVER["DOCUMENT_ROOT"]. '/P5Crits/portal/models/UserManager.php');
 
 
 $_FileManager = new FileManager;
@@ -13,9 +13,31 @@ $userId = $_SESSION['admin']['id'];
 $docFiles = $_FileManager->getFiles($table, $userId, $pageDocFile, $entiteParPage);
 $docFilePages = $_FileManager->getPageMax($table, $entiteParPage, $userId);
 
-if(isset($_POST['action']) && $_POST['action']=='showDocFiles'){
-    $docFileOutput=''; 
+if(isset($_POST['action']) && $_POST['action']=='addDoc'){
+   //echo 'fichier nom :' .$_FILES['file']['name'];
+   /* $filename = $_FILES['file']['name'];
+    var_dump($filename);
+    $filesize = $_FILES['file']['size'];
+    var_dump($filesize);
+  
+    $location = "upload/".$filename;
 
+    $return_arr = array();
+
+    if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
+        $src = "default.png";
+
+        // checking file is image or not
+        if(is_array(getimagesize($location))){
+            $src = $location;
+        }
+        $return_arr = array("name" => $filename,"size" => $filesize, "src"=> $src);
+    }
+    
+    $responseDocFile =  json_encode($return_arr);
+    var_dump($responseDocFile);
+    //exit($responseDocFile);
+    /*$docFileOutput=''; 
     foreach ($docFiles as $docFile){
         $docFileOutput.='<tr class="docFileRow' .$docFile->id(). '">';
         $docFileOutput.='<td>' .$docFile->id(). '</td>';
@@ -32,7 +54,8 @@ if(isset($_POST['action']) && $_POST['action']=='showDocFiles'){
     $data['pageDocFile'] = $pageDocFile;
     $data['docFilePages'] = $docFilePages;
     $responsedocFile = json_encode($data);
-    exit($responsedocFile);
+    exit($responsedocFile);*/
+    
 }
 
 if(isset($_POST['action']) && $_POST['action']=='updateDocFile'){

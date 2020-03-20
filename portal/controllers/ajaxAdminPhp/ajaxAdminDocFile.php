bc8c9785a39a00f2950132954516b3767ef5cfb8
LudFav <45117462+LudFav@users.noreply.github.com>
2020-03-20 11:24:42 +0100

    Update ajaxAdminDocFile.php

--- portal/controllers/ajaxAdminPhp/ajaxAdminDocFile.php
+++ portal/controllers/ajaxAdminPhp/ajaxAdminDocFile.php
@@ -13,50 +13,6 @@ $userId = $_SESSION['admin']['id'];
 $docFiles = $_FileManager->getFiles($table, $userId, $pageDocFile, $entiteParPage);
 $docFilePages = $_FileManager->getPageMax($table, $entiteParPage, $userId);
 
-if(isset($_POST['action']) && $_POST['action']=='addDoc'){
-   //echo 'fichier nom :' .$_FILES['file']['name'];
-   /* $filename = $_FILES['file']['name'];
-    var_dump($filename);
-    $filesize = $_FILES['file']['size'];
-    var_dump($filesize);
-  
-    $location = "upload/".$filename;
-
-    $return_arr = array();
-
-    if(move_uploaded_file($_FILES['file']['tmp_name'],$location)){
-        $src = "default.png";
-
-        // checking file is image or not
-        if(is_array(getimagesize($location))){
-            $src = $location;
-        }
-        $return_arr = array("name" => $filename,"size" => $filesize, "src"=> $src);
-    }
-    
-    $responseDocFile =  json_encode($return_arr);
-    var_dump($responseDocFile);
-    //exit($responseDocFile);
-    /*$docFileOutput=''; 
-    foreach ($docFiles as $docFile){
-        $docFileOutput.='<tr class="docFileRow' .$docFile->id(). '">';
-        $docFileOutput.='<td>' .$docFile->id(). '</td>';
-        $docFileOutput.='<td>' .$docFile->name(). '</td>';
-        $docFileOutput.='<td>' .$docFile->name(). '</td>';
-        $docFileOutput.='<td>' .$docFile->date(). '</td>';
-        $docFileOutput.='<td class="docFileActionTd">';       
-        $docFileOutput.='<button class="updateDocFile" value="' .$docFile->id(). '" data-toggle="modal" data-target ="#updateDocFileModal" ></button>';
-        $docFileOutput.='<button class="deleteDocFile" value="' .$docFile->id(). '" data-toggle="modal" data-target ="#deleteDocFileModal" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
-        $docFileOutput.='</td>';
-        $docFileOutput.='</tr>';
-    }
-    $data['docFileOutput'] = $docFileOutput;
-    $data['pageDocFile'] = $pageDocFile;
-    $data['docFilePages'] = $docFilePages;
-    $responsedocFile = json_encode($data);
-    exit($responsedocFile);*/
-    
-}
 
 if(isset($_POST['action']) && $_POST['action']=='updateDocFile'){
     $_fileManager->update($_POST['docFile2update']); 

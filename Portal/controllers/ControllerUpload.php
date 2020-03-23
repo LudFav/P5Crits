07c08408a21bbcef6ddae6ec0909d82e71a4bdce
LudFav <45117462+LudFav@users.noreply.github.com>
2020-03-23 12:25:07 +0100

    Update ControllerUpload.php

--- Portal/controllers/ControllerUpload.php
+++ Portal/controllers/ControllerUpload.php
@@ -16,36 +16,59 @@ class ControllerUpload{
 
   function docFileShow(){
     $output='';
-    $messageOutput='';
-    $docpath = "documents/".$_SESSION['admin']['id'];
-    $tempdocpath = "temp/".$_SESSION['admin']['id'];
-    if(isset($_POST['action']) && $_POST['action']=='uploadDocFiles'){
-      return "ok";
-      var_dump($tempLocation, $location);
-
-      if(rename ( $tempLocation , $location )){
-          $src = "default.png";
-          if(is_array(getimagesize($location))){
-              $src = $location;
-              $messageOutput.='<p class="uploadOk">Document(s) envoyé(s)</p>';
-          }
-          $return_arr[] = array("name" => $filename,"size" => $filesize, "src"=> $src);   
-      }
-      $data= $messageOutput;
-      return $data;
-      
-    }  else if(isset($_FILES['file']['name'][0])){
+    
+    
+   if(isset($_FILES['file']['name'][0])){
       $countfiles = count($_FILES['file']['name']);
       $return_arr = array();
+      $date = new \DateTime();
       for($i=0;$i<$countfiles;$i++){
-        $filename = $_FILES['file']['name'][$i];
+        $timeStp = $date->getTimestamp();
+        $file = $_FILES["file"]["name"][$i];
+        $array = explode('.', $file);
+        $fileTitle = $array[0];
+        $fileExtension = $array[1];
+        $filename = $fileTitle."_".$timeStp.".".$fileExtension;
         $filesize = $_FILES['file']['size'][$i];
-      
-        move_uploaded_file($_FILES['file']['tmp_name'][$i], $tempLocation);
-        $output.= '<div id="thumbnail_'.$filename.'" class="thumbnail">';
-        $output.= '<img src="'.$tempLocation.'" width="100%" height="78%">';
-        $output.= '<span class="size">'.$filename.'<span></br>';
-        $output.= '</div>';
+        $docpath = "documents/".$_SESSION['admin']['id'];
+        if( is_dir($docpath) === false ){
+          mkdir($docpath);
+        }
+        $filepath = $docpath .'/ '. $filename; 
+        $return_arr[] = array("name" => $filename,"size" => $filesize, "src"=> $filepath);
+        $finfo = finfo_open( FILEINFO_MIME_TYPE );
+        $tmpname =  $_FILES['file']['tmp_name'][$i];
+        $mtype = finfo_file( $finfo, $tmpname );
+	      finfo_close( $finfo );
+        if ( $mtype == "application/pdf") {
+          move_uploaded_file($_FILES['file']['tmp_name'][$i], $filepath);
+          $output.= '<div id="thumbnail_'.$filename.'" class="thumbnail">';
+          $output.= '<img src="documents/default.png" width="100%" height="78%">';
+          $output.= '<span class="size">'.$filename.'<span></br>';
+          $output.= '</div>';
+          
+        } else if($mtype == "image/png"  ||
+	           $mtype ==  "image/jpeg"  || 
+	           $mtype == "image/gif" || 
+	           $mtype ==  "image/psd"  || 
+             $mtype == "image/bmp" 
+        ) {
+           move_uploaded_file($_FILES['file']['tmp_name'][$i], $filepath);
+           $output.= '<div id="thumbnail_'.$filename.'" class="thumbnail">';
+           $output.= '<img src="'.$filepath.'" width="100%" height="78%">';
+           $output.= '<span class="size">'.$filename.'<span></br>';
+           $output.= '</div>';
+          } else if ( $filesize  > 800000){
+              $output.= '<div id="thumbnail_'.$filename.'" class="thumbnail">';
+              $output.= '<img src="documents/error.png" width="100%" height="78%">';
+              $output.= '<span class="size" style="color:red;">'.$filename.' Depasse la limite de 5Mo<span></br>';
+              $output.= '</div>';
+              } else {
+              $output.= '<div id="thumbnail_'.$filename.'" class="thumbnail">';
+              $output.= '<img src="documents/error.png" width="100%" height="78%">';
+              $output.= '<span class="size" style="color:red;">'.$filename.' Mauvais type de fichier<span></br>';
+              $output.= '</div>';
+              }
       }
       $data['output'] = $output;
       $data['return_arr'] = $return_arr;

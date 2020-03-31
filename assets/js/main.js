a1f2c98fa17cbac71997f950011e83eb0ecab324
LudFav <45117462+LudFav@users.noreply.github.com>
2020-03-31 12:18:39 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,17 +117,6 @@
 			}, 100);
 		});
 
-	// Change Button
-	$(document).ready(function() {
-		var url_param = $(location).attr('href').split("/");
-		var param = (url_param[4]).split("&")
-		var whatIwant = (param[0]);
-		if(whatIwant === 'sommaire'){
-			let accueil = '<a href="accueil" class="fas fa-angle-left" id="accueil"></a><span class="label"></span></a>';
-			$('#retourSommaire').replaceWith($(accueil));
-		}
-	});
-
 	// Scrolly.
 		$('.scrolly').scrolly();
 

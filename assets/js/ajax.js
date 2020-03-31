2a93d2b837c35292717d7e621802be918a2765a1
LudFav <45117462+LudFav@users.noreply.github.com>
2020-03-31 08:19:36 +0200

    Update ajax.js

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -11,7 +11,7 @@ function billetAccueil() {
             responseBilletAccueil = JSON.parse(data);
             billetAccueilTable = responseBilletAccueil.billetsAccueilOutput;
             accueilMaxPages = responseBilletAccueil.maxPages;
-            billetAccueilPagination = new FrontPagination(
+            billetAccueilPagination = new Pagination(
                 "#paginationAccueil",
                 "pageAccueilBillet",
                 accueilMaxPages,
@@ -57,7 +57,7 @@ function showComment() {
             responseFrontCom = JSON.parse(data);
             responseFrontComTable = responseFrontCom.commentairesOutput;
             frontComPagesMax = responseFrontCom.maxPagesComFront;
-            frontComPagination = new FrontPagination(
+            frontComPagination = new Pagination(
                 "#paginationFrontCom",
                 "frontComPage",
                 frontComPagesMax,
@@ -163,96 +163,6 @@ function signalement(id) {
         }
     });
 }
-
-//OBJET PAGINATION***********************************************************
-function FrontPagination(element, paginationId, pagesMax, pageName) {
-    this.element = element;
-    this.paginationId = paginationId;
-    this.pagesMax = pagesMax;
-    this.pageName = pageName;
-    $(element).html("");
-    numPage = pageName;
-    pagesmax = pagesMax;
-    pageNav = 2;
-
-    let pagination = $(
-        '<nav aria-label="Page navigation ' +
-        paginationId +
-        '" id="' +
-        paginationId +
-        '"></nav>'
-    ).appendTo($(element));
-    let paginationUl = $(
-        '<ul class="' + paginationId + ' paginationUl"></ul>'
-    ).appendTo($(pagination));
-
-    let paginationPrev = $(
-        '<li class="page-item"><button class="' +
-        paginationId +
-        ' page-link prev">Previous</button></li>'
-    ).appendTo($(paginationUl));
-
-    for (let i = numPage - pageNav; i < numPage; i++) {
-        if (i > 0) {
-            let leftPage = $(
-                '<li class="page-item"><a class="' +
-                paginationId +
-                ' page-link but left" value=' +
-                i +
-                ">" +
-                i +
-                "</a></li>"
-            );
-            $(leftPage).appendTo($(paginationUl));
-        }
-    }
-
-    let currentPage = $(
-        '<li class="page-item current"><p class="' +
-        paginationId +
-        '  page-link active" value="' +
-        numPage +
-        '">' +
-        numPage +
-        "</p></li>"
-    ).appendTo($(paginationUl));
-
-    for (let j = numPage + 1; j <= pagesMax; j++) {
-        let rightPage = $(
-            '<li class="page-item"><a class="' +
-            paginationId +
-            ' page-link but right" value=' +
-            j +
-            ">" +
-            j +
-            "</a></li>"
-        );
-        $(rightPage).appendTo($(paginationUl));
-        if (j >= numPage + pageNav) {
-            break;
-        }
-    }
-
-    let paginationNext = $(
-        '<li class="page-item"><button class="' +
-        paginationId +
-        ' page-link next">Next</button></li>'
-    ).appendTo(paginationUl);
-
-    $(paginationPrev).hide();
-    if (pageName > 1) {
-        $(paginationPrev).show();
-    } else {
-        $(paginationPrev).hide();
-    }
-
-    if (pageName >= pagesmax) {
-        $(paginationNext).hide();
-    } else {
-        $(paginationNext).show();
-    }
-}
-
 //BOUTON SIGNALER
 $(window).bind('load', function () {
 

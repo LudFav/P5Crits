42697ae051e66274663db801ec38d7f0d2e9b65c
LudFav <45117462+LudFav@users.noreply.github.com>
2020-04-03 15:48:01 +0200

    Update template.php

--- Portal/views/template.php
+++ Portal/views/template.php
@@ -8,7 +8,7 @@
 	<head>
 		<title><?= $title?></title>
 		<meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         <!--<link href="assets/css/main.css" rel="stylesheet"/>-->
         <link href="assets/css/main2.css" rel="stylesheet"/>

054fcd5244f7bea0bea571eebc426e22f2fb8e9f
LudFav <45117462+LudFav@users.noreply.github.com>
2020-04-03 15:47:42 +0200

    Update ajaxAdminBillet.php

--- Portal/controllers/ajaxAdminPhp/ajaxAdminBillet.php
+++ Portal/controllers/ajaxAdminPhp/ajaxAdminBillet.php
@@ -23,9 +23,9 @@ if(isset($_POST['action']) && $_POST['action']=='showbillet'){
          $billetsOutput.= '<td>' .$billet->titre(). '</td>';
          $billetsOutput.= '<td>' .$billet->date(). '</td>';
          $billetsOutput.= '<td class="actionTd" value="' .$pages. '">';
-         $billetsOutput.= '<button class="visualBilBtn"><a href="post&id=' .$billet->id(). '" ><i class="fa fa-eye"></i></a></button>';
-         $billetsOutput.= '<button class="editBilBtn" ><a href="update&id=' .$billet->id(). '"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>';
-         $billetsOutput.= '<button class="deleteBilBtn" value="' .$billet->id(). '" data-toggle="modal" data-target ="#deleteBillet" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
+         $billetsOutput.= '<button class="admin-btn visualBilBtn"><a href="post&id=' .$billet->id(). '" ><i class="fa fa-eye"></i></a></button>';
+         $billetsOutput.= '<button class="admin-btn editBilBtn" ><a href="update&id=' .$billet->id(). '"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>';
+         $billetsOutput.= '<button class="admin-btn deleteBilBtn" value="' .$billet->id(). '" data-toggle="modal" data-target ="#deleteBillet" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
          $billetsOutput.= '</td>';
          $billetsOutput.= '</tr>'; 
     }

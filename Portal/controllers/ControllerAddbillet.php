7a66eada9ebe46f47f2afff999e2227b1cf40e1f
LudFav <45117462+LudFav@users.noreply.github.com>
2020-04-04 20:34:45 +0200

    Update ControllerAddbillet.php

--- Portal/controllers/ControllerAddbillet.php
+++ Portal/controllers/ControllerAddbillet.php
@@ -1,5 +1,6 @@
 <?php
 namespace CritsPortal\controllers;
+
 require_once($_SERVER['DOCUMENT_ROOT']. '/P5Crits/vendor/autoload.php');
 
 class ControllerAddbillet
@@ -14,18 +15,29 @@ class ControllerAddbillet
   }
 
   private function addBillet(){
-    
+ 
     if(isset($_POST['add'])){
-      if(!empty($_POST['auteur']) && !empty($_POST['titre']) && !empty($_POST['contenu']) ){
+      if(!empty($_POST['auteur']) && !empty($_POST['titre']) && !empty($_POST['contenu']) && !empty($_POST['idImage']) ){
+        $this->_fileManager = new \CritsPortal\models\FileManager();
+        $idImage = $_POST['idImage'];
+        $image = $this->_fileManager->getFile('imgfile', $idImage);
+        var_dump($idImage);
+        var_dump($image[0]);
+        if($idImage == NULL){
+          $imageUrl = 'assets/images/Critstest.png';
+        } else {
+          $imageUrl = $image['file_url'];
+        }
+       
         $data = array(
         'auteur' => htmlspecialchars($_POST['auteur']),
         'titre' => htmlspecialchars($_POST['titre']),
-        'image' => $_POST['image'],
+        'image' => $imageUrl,
         'contenu' =>  $_POST['contenu']
         );
         $this->_billetManager = new \CritsPortal\models\BilletManager();
         $billets = $this->_billetManager->createBillet($data);
-        header('Location:admin');
+        //header('Location:admin');
       } else {
         return false;
       }

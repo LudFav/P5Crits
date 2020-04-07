51fdff809564a2457fd7f2bc4daf0fc0ee5e7f47
LudFav <45117462+LudFav@users.noreply.github.com>
2020-04-07 23:40:51 +0200

    Update ajaxAdminBillet.php

--- Portal/controllers/ajaxAdminPhp/ajaxAdminBillet.php
+++ Portal/controllers/ajaxAdminPhp/ajaxAdminBillet.php
@@ -1,10 +1,10 @@
 <?php
 use CritsPortal\models\BilletManager;
-require_once($_SERVER["DOCUMENT_ROOT"]. '/P5Crits/portal/models/BilletManager.php');
+use CritsPortal\models\FileManager;
+require_once($_SERVER["DOCUMENT_ROOT"]. '/P5Crits/Portal/models/BilletManager.php');
 
 $_billetManager;
 $_billetManager = new BilletManager; 
-
 $entiteParPage = 4;
 $nbreEntitesParPage = $entiteParPage;
 
@@ -21,11 +21,12 @@ if(isset($_POST['action']) && $_POST['action']=='showbillet'){
          $billetsOutput.= '<a href="post&id' .$billet->id(). '"><td>' .$billet->id(). '</td></a>';
          $billetsOutput.= '<td>' .$billet->auteur(). '</td>';
          $billetsOutput.= '<td>' .$billet->titre(). '</td>';
+         $billetsOutput.= '<td class="thumbnail file"><img src="' .$billet->image(). '" ></td>';
          $billetsOutput.= '<td>' .$billet->date(). '</td>';
          $billetsOutput.= '<td class="actionTd" value="' .$pages. '">';
-         $billetsOutput.= '<button class="visualBilBtn"><a href="post&id=' .$billet->id(). '" ><i class="fa fa-eye"></i></a></button>';
-         $billetsOutput.= '<button class="editBilBtn" ><a href="update&id=' .$billet->id(). '"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>';
-         $billetsOutput.= '<button class="deleteBilBtn" value="' .$billet->id(). '" data-toggle="modal" data-target ="#deleteBillet" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
+         $billetsOutput.= '<button class="admin-btn visualBilBtn"><a href="post&id=' .$billet->id(). '" ><i class="fa fa-eye"></i></a></button>';
+         $billetsOutput.= '<button class="admin-btn editBilBtn" ><a href="update&id=' .$billet->id(). '"><i class="fa fa-pencil" aria-hidden="true"></i></a></button>';
+         $billetsOutput.= '<button class="admin-btn deleteBilBtn" value="' .$billet->id(). '" data-toggle="modal" data-target ="#deleteBillet" ><i class="fa fa-trash" aria-hidden="true"></i></button>';
          $billetsOutput.= '</td>';
          $billetsOutput.= '</tr>'; 
     }
@@ -39,5 +40,30 @@ if(isset($_POST['action']) && $_POST['action']=='showbillet'){
 //EFFACER BILLET
 if(isset($_POST['action']) && $_POST['action']=='deleteBillet'){
     $deleteBillet = $_billetManager->deleteBillet($_POST['deleteBillet']); 
+}
+
+
+//MODAL IMAGE ADDBILLET FORM
+$_fileManager;
+$_fileManager = new FileManager;
+$userId = $_SESSION['admin']['id'];
+$pageImg = isset($_POST['pageImg'])? $_POST['pageImg'] :1;
+$entiteImgParPage = 6;
+$imageBillet =  $_fileManager->getFiles('imgfile', $userId, $pageImg, $entiteImgParPage);
+$pageMaxImg = $_fileManager->getPageMax('imgfile', $entiteImgParPage, $userId);
+$imageOutput ='';
+if(isset($_POST['action']) && $_POST['action']=='showImageLib'){
+    //$image = isset($_POST['idImage'])? $_POST['idImage'] :1;
+    foreach($imageBillet as $image){
+    $imageOutput .= '<input type="radio" name="choice' .$image->id(). '" class=" sr-only imgRadioImpt" id="' .$image->type().$image->id(). '" value="' .$image->id(). '">';
+    $imageOutput .= '<label class="thumbnail imgRadioLbl" for="choice' .$image->id(). '" value="' .$image->id(). '" >';
+    $imageOutput .= '<img src="' .$image->file_url(). '" alt="' .$image->name(). '" class="file' .$image->id(). '">';
+    $imageOutput .= '</label>';
+    }
+    $data['imageOutput']=$imageOutput;
+    $data['pageImg']=$pageImg;
+    $data['pageMaxImg']=$pageMaxImg;
+    $responseImgBillet = json_encode($data);
+    exit($responseImgBillet);
 }
 ?>

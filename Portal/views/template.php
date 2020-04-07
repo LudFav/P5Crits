e49d85fa35d6834be8162aee4e8046805ee40aae
LudFav <45117462+LudFav@users.noreply.github.com>
2020-04-07 22:42:09 +0200

    Update template.php

--- Portal/views/template.php
+++ Portal/views/template.php
@@ -27,7 +27,7 @@
                         </div>
                         <!-- Copyright -->
                         <div id="copyright">
-                            <ul><li>&copy; Untitled</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
+                            <ul><li>&copy; CRITS</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
                         </div>
     
                 </div>

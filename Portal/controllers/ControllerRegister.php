aa6e627ca7adad01df6856349ea488473a4cf3ae
LudFav <45117462+LudFav@users.noreply.github.com>
2021-02-04 08:49:36 +0100

    Update ControllerRegister.php

--- Portal/controllers/ControllerRegister.php
+++ Portal/controllers/ControllerRegister.php
@@ -2,7 +2,7 @@
 namespace CritsPortal\controllers;
 
 require_once($_SERVER['DOCUMENT_ROOT']. '/P5Crits/vendor/autoload.php');
-
+require_once $_SERVER["DOCUMENT_ROOT"]. '/P5Crits/Portal/controllers/ajaxClientPhp/ajaxClientRegister.php';
 class ControllerRegister
 {
   private $_userManager;
@@ -13,9 +13,8 @@ class ControllerRegister
     $this->_userManager = new \CritsPortal\models\UserManager; 
     $this->_view = new \CritsPortal\views\View('Register');
     $this->_view->generate('inscription', array()); 
-    $this->addUser();
   }
-
+/*
   private function addUser(){
     if(isset($_POST['action']) && $_POST['action']=='register'){
       if(!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])){
@@ -25,44 +24,45 @@ class ControllerRegister
         'email' => trim(htmlspecialchars($_POST['email'])),
         'password' => trim(htmlspecialchars($_POST['password'])),
         'confirmedPassword' => trim(htmlspecialchars($_POST['confirmedPassword'])),
-        'role' => 'user',
-        'error' => ''
+        'role' => 'user'
         );
 
         //Validation du nom : il peut comporter des lettres et des numeros
         $nameValidation= "/^[a-zA-Z0-9]*$/"; 
         if(!preg_match($nameValidation, $data['username'])){
-          $data['error'] ='usernameError';
+          $error ='usernameError';
         }
 
         //Validation Email: vérification des caracteres utilisés et que l'email n'existe pas deja dans la bdd
         $email = $data['email'];
         if(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
-          $data['error'] ='mailInvald';
+          $error ='mailInvald';
         }elseif($this->_userManager->getUserEmail($email)){
-          $data['error'] ='mailAlreadyUsed';
+          $error ='mailAlreadyUsed';
         }
 
         //Mot de passe et mot de passe de confirmation
         $passValidation = "/^(.{0,7}|[^a-z]*|[^\d]*)$/i";
         if(preg_match($passValidation, $data['password'])){
-          $data['error'] = 'passWithoutNumber';
+          $error = 'passWithoutNumber';
         } 
         if(strlen($data['password']) <= 7){
-          $data['error'] = 'passTooShort';
+          $error = 'passTooShort';
         }
         if ($data['password'] != $data['confirmedPassword']) {
-          $data['error'] = 'passUnmatched';
+          $error = 'passUnmatched';
         }
 
         //Si aucune erreur on créer le compte sinon on reste sur la page
-        if (empty($data['error'])){
+        if (empty($error)){
           $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT, ['cost' => 12]);
           $users = $this->_userManager->createUser($data);
-          header('Location:accueil');
-          } else{
-              echo $data['error'];
-              exit();
+          exit;
+          } else{   
+              $data['error'] = $error;
+              $responseError = json_encode($data['error']);
+              ob_clean();
+              exit($responseError);
           }
           // POST IT creer une vue/session USER
         } else {
@@ -70,4 +70,5 @@ class ControllerRegister
         }
     } 
   }
+     */
 }

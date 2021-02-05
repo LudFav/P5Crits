0702b0e358f726801a6a98e563914a87429ef586
LudFav <45117462+LudFav@users.noreply.github.com>
2021-02-05 08:34:15 +0100

    Update template.php

--- Portal/views/template.php
+++ Portal/views/template.php
@@ -16,6 +16,15 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
+     <!-- Nav -->
+     <nav id="nav">
+            <ul class="links">
+                <li class="active"><a href="accueil">Accueil</a></li>
+                <li><a href="sommaire">Sommaire</a></li>
+                <li><a href="" id="login" data-toggle="modal" data-target="#connexion">Connexion</a></li>
+                <!--li a >adresse a remplir vers dernier article php-->
+            </ul>
+        </nav>
     <!--Content-->
     <?= $content ?>	
     	<!-- Footer -->

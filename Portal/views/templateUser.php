519323ddb8062930cd1e281a5e20ba2d6ee51734
LudFav <45117462+LudFav@users.noreply.github.com>
2021-02-05 08:34:20 +0100

    Update templateUser.php

--- Portal/views/templateUser.php
+++ Portal/views/templateUser.php
@@ -4,11 +4,11 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
-<html>
+<html lang="fr">
 	<head>
 		<title><?= $title?></title>
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="assets/css/main.css" rel="stylesheet"/>
 		<link href="assets/css/bootstrap.css" rel="stylesheet">
 		<link href="assets/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet">
@@ -16,48 +16,30 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
+     <!-- Nav -->
+     <nav id="nav">
+            <ul class="links">
+                <li class="active"><a href="accueil">Accueil</a></li>
+                <li><a href="sommaire">Sommaire</a></li>
+                <li><a href="profil">Profil</a></li>
+                <li><a class="logout" id="logout">Déconnexion</a></li>
+                <!--li a >adresse a remplir vers dernier article php-->
+            </ul>
+        </nav>
     <!--Content-->
     <?= $content ?>	
     	<!-- Footer -->
         <footer id="footer">
 						
                         </footer>
-                        <div class="container">
-                            <ul class="menu admin">
-                                <li><a href="admin" class="fas fa-feather-alt" id="admin"> Administration</a></li>
-                                <li><a href="accueil" class="fas fa-book" id="retourSommaire"> Accueil du site</a></li>
-                                <li><a class="logout fas fa-sign-out-alt" id="logout"> Deconnexion</a></li>
-                            </ul>
-                        </div>
-                    <!-- Copyright -->
+                        <!-- Copyright -->
                         <div id="copyright">
                             <ul><li>&copy;CRITS - 2020</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
                         </div>
     
                 </div>
     
-            <!-- Scripts -->            
-                <script>window.onload = function() { document.body.classList.remove('is-preload'); }
-                	window.ontouchmove = function() { return false; }
-                	window.onorientationchange = function() { document.body.scrollTop = 0; }
-                </script>
-                <script src="assets/js/jquery-3.4.1.min.js"></script>
-                <script src="assets/js/tether.min.js"></script>
-                <script src="assets/js/bootstrap.js"></script>
-                <script src="assets/js/tinymce/tinymce.min.js"></script>
-                <script> tinymce.init({
-                  selector: '#textareaB, #textareaC, #textareaEditC',  // change this value according to your HTML
-                  plugin: 'a_tinymce_plugin',
-                  a_plugin_option: true,
-                  content_style: 'body{font-family:book antiqua;}',
-                  a_configuration_option: 400,
-                  setup: function (editor) {
-                	editor.on('change', function (e) {
-                	tinymce.triggerSave();
-                	});
-                }
-                });
-                </script>
+            <!-- Scripts -->
                 <script src="assets/js/jquery.min.js"></script>
                 <script src="assets/js/jquery.scrollex.min.js"></script>
                 <script src="assets/js/jquery.scrolly.min.js"></script>
@@ -69,12 +51,7 @@
                 <script src="assets/js/bootstrap.js"></script>
                 <script src="assets/js/Modal.js"></script>
                 <script src="assets/js/Pagination.js"></script>
-                <script src="assets/js/adminBillet.js"></script>
-                <script src="assets/js/adminComMod.js"></script>
-                <script src="assets/js/adminComSign.js"></script>
-                <script src="assets/js/adminMedia.js"></script>
-                <script src="assets/js/adminAjoutImage.js"></script>
-                <script src="assets/js/ajaxAdmin.js"></script>
+                <script src="assets/js/ajaxUser.js"></script>
                 <script src="assets/js/ajax.js"></script>
                 <script src="assets/js/main.js"></script>
         </body>

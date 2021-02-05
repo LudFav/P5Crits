f0752e46973e5df088492747ba4923d70eee91ed
LudFav <45117462+LudFav@users.noreply.github.com>
2021-02-05 08:34:01 +0100

    Update ajaxAdmin.js

--- assets/js/ajaxAdmin.js
+++ assets/js/ajaxAdmin.js
@@ -1,98 +1,95 @@
 function logout() {
-  $.post({
-    url: "login",
-    data: { action: "logout" },
-    success: function(data) {
-      window.location.href = data;
-    }
-  });
+    $.post({
+        url: "login",
+        data: { action: "logout" },
+        success: function(data) {
+            window.location.href = data;
+        }
+    });
 }
 
 function isLoggedin() {
-  $.post({
-    url: "login",
-    data: { action: "isLogged" },
-    success: function(data) {
-      if (data == "1") {
-        let logout = '<a class="logout fas fa-sign-out-alt"></a>';
-        $('#login').replaceWith($(logout));
-        let adminButton = '<li><a href="admin" class="fas fa-feather-alt"></a></li>';
-        $(adminButton).insertBefore($('li.logout'));
-      }
-    }
-  });
+    $.post({
+        url: "login",
+        data: { action: "isLogged" },
+        success: function(data) {
+            if (data == "1") {
+                let logout = '<a href="" class="logout">Déconnexion</a>';
+                $('#login').replaceWith($(logout));
+                let adminButton = '<li><a href="admin" class="fas fa-feather-alt"></a></li>';
+                $(adminButton).insertBefore($('li.logout'));
+            }
+        }
+    });
 }
 
 //FONCTIONS REQUETES AJAX ACTIONS*****************************************
 
 
-function validateEdit(){
-    $("#formEditBillet").on('submit', function(){
-    if(!$("#textareaEditC").val() == true){
-      $('#alertModal').modal('show');
-      return false 
-    }  
-    return true;
+function validateEdit() {
+    $("#formEditBillet").on('submit', function() {
+        if (!$("#textareaEditC").val() == true) {
+            $('#alertModal').modal('show');
+            return false
+        }
+        return true;
     })
 }
 
-function validateAdd(){
-  $("#formAddBillet").on('submit', function(){
-    if(!$("#textareaB").val() == true){
-      $('#alertModal').modal('show');
-      return false 
-    }  
-    return true;
+function validateAdd() {
+    $("#formAddBillet").on('submit', function() {
+        if (!$("#textareaB").val() == true) {
+            $('#alertModal').modal('show');
+            return false
+        }
+        return true;
     })
 }
 
 //MODAL UPDATE BILLET
-modalAlertUpdate =  new Modal(document.querySelector("body"), {
-  id: "alertModal",
-  titre: "Probleme",
-  type: "alert",
-  message: "Vous n'avez pas remplie tout les champs"
+modalAlertUpdate = new Modal(document.querySelector("body"), {
+    id: "alertModal",
+    titre: "Probleme",
+    type: "alert",
+    message: "Vous n'avez pas remplie tout les champs"
 });
 
 
 
 //MODAL UPLOAD
 modalDocFile = new Modal(document.querySelector("body"), {
-  id: "docFileModal",
-  titre: "Upload de fichier",
-  type: "upload",
-  message: "Type de fichier attendu :</br> pdf, jpg, jpeg, png, gif, bmp</br>Taille max : 5mo"
+    id: "docFileModal",
+    titre: "Upload de fichier",
+    type: "upload",
+    message: "Type de fichier attendu :</br> pdf, jpg, jpeg, png, gif, bmp</br>Taille max : 5mo"
 });
 
 modalimgFile = new Modal(document.querySelector("body"), {
-  id: "imgFileModal",
-  titre: "Upload d'image",
-  type: "upload",
-  message: "type de fichier image attendu : jpg jpeg png gif</br> ou</br> JPG JPEG PNG GIF</br>Taille max : 5mo"
+    id: "imgFileModal",
+    titre: "Upload d'image",
+    type: "upload",
+    message: "type de fichier image attendu : jpg jpeg png gif</br> ou</br> JPG JPEG PNG GIF</br>Taille max : 5mo"
 });
 
 //BOUTONS CONFIRMATION MODAL*************************************************
 $(window).bind("load", function() {
 
-$(".logout").on("click", function() {
-  logout();
-});
- 
-//BOUTONS NOUVEAU DOC
-$("#docFileModal-validBtn").on("click", function(){
-  showFile()
-})
+    $(".logout").on("click", function() {
+        logout();
+    });
+
+    //BOUTONS NOUVEAU DOC
+    $("#docFileModal-validBtn").on("click", function() {
+        showFile()
+    })
 });
 
-$(".editBillet").on("click", function(){
-  return validateEdit();
+$(".editBillet").on("click", function() {
+    return validateEdit();
 })
 
-$(".addBillet").on("click", function(){
-  return validateAdd();
+$(".addBillet").on("click", function() {
+    return validateAdd();
 })
 
 isLoggedin();
-
-
-

444166c0ca3fbddd60cbc0bf4dd4d386c9f9abc1
LudFav <45117462+LudFav@users.noreply.github.com>
2021-03-18 15:58:35 +0100

    Update ControllerProfil.php

---
+++ Portal/controllers/ControllerProfil.php
@@ -0,0 +1,48 @@
+<?php
+namespace CritsPortal\controllers;
+
+require_once $_SERVER["DOCUMENT_ROOT"]. '/P5Crits/Portal/controllers/ajaxClientPhp/ajaxClientRegister.php';
+
+class ControllerProfil {
+  private $_userManager;
+  private $_view;
+
+  public function __construct(){
+    require_once('Portal/controllers/Router.php');
+    if (isset($url) && count($url) < 1) {
+      throw new \Exception("Page Introuvable");
+    } else {
+      $this->profil();    
+    }
+  }
+
+  private function profil(){
+    if(isset($_SESSION[$userRole]) && !empty($_SESSION[$userRole])){
+    //username  
+        
+        $username = $_SESSION[$userRole]['username'];
+        /**
+         * Profil a montrer
+         */
+          $this->_userManager = new \CritsPortal\models\UserManager;
+          $user = $this->_userManager->getUser($username);
+        
+        //VUE
+            /**
+         * Generation de la vue 
+         */
+        
+            if($user == false){
+              $errorMsg = 'Profil introuvable';
+              $this->_view = new \CritsPortal\views\View('Error');
+              $this->_view->generate('Erreur', array('errorMsg' => $errorMsg));
+            } else{
+              $this->_view = new \CritsPortal\views\View('Profil');
+              $this->_view->generate('Crits-Profil', array('user' => $user));
+            }
+
+        }
+    
+  }
+}
+

96f1b9e0e8fed26b564571a4d8eb766d9f4a19fd
LudFav <45117462+LudFav@users.noreply.github.com>
2021-03-23 13:57:05 +0100

    Update Model.php

--- Portal/models/Model.php
+++ Portal/models/Model.php
@@ -9,8 +9,8 @@ abstract class Model
   //connexion a la bdd
 
   private static function setBdd(){
-    self::$_bdd = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-    //self::$_bdd = new \PDO('mysql:host=db5000350603.hosting-data.io;dbname=dbs340719;charset=utf8', 'dbu494207', 'P5%Roll20LikeApp');
+    //self::$_bdd = new \PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+    self::$_bdd = new \PDO('mysql:host=db5000350603.hosting-data.io;dbname=dbs340719;charset=utf8', 'dbu494207', 'P5%Roll20LikeApp');
     //on utilise les constantes de PDO pour gérer les erreurs
     self::$_bdd->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_WARNING);
   }

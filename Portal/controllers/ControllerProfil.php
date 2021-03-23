a052fd5150be0ff64384a0b9784f463316c73a6e
LudFav <45117462+LudFav@users.noreply.github.com>
2021-03-23 15:06:30 +0100

    Update ControllerProfil.php

--- Portal/controllers/ControllerProfil.php
+++ Portal/controllers/ControllerProfil.php
@@ -1,48 +1,23 @@
 <?php
 namespace CritsPortal\controllers;
+require_once($_SERVER['DOCUMENT_ROOT']. '/P5Crits/vendor/autoload.php');
+require_once $_SERVER["DOCUMENT_ROOT"]. '/P5Crits/Portal/controllers/ajaxClientPhp/ajaxClientCom.php';
+use CritsPortal\views\View;
 
-require_once $_SERVER["DOCUMENT_ROOT"]. '/P5Crits/Portal/controllers/ajaxClientPhp/ajaxClientRegister.php';
-
-class ControllerProfil {
-  private $_userManager;
-  private $_view;
-
-  public function __construct(){
-    require_once('Portal/controllers/Router.php');
-    if (isset($url) && count($url) < 1) {
-      throw new \Exception("Page Introuvable");
+class ControllerProfil{
+    private $_view;
+    public function __construct(){
+  
+      if(isset($_SESSION['id']) && !empty($_SESSION['id'])){
+        $this->_userManager = new \CritsPortal\models\UserManager;
+        var_dump($_SESSION['username']);
+        var_dump($_SESSION['role']);
+        $userName=$_SESSION['username'];
+        $user = $this->_userManager->getUser($userName);
+        $this->_view = new View('Profil'); 
+        $this->_view->generate('Profil', array('user' => $user));
     } else {
-      $this->profil();    
+      header('Location:accueil');
     }
   }
-
-  private function profil(){
-    if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
-    //username  
-        
-        $username = $_SESSION['username'];
-        /**
-         * Profil a montrer
-         */
-          $this->_userManager = new \CritsPortal\models\UserManager;
-          $user = $this->_userManager->getUser($username);
-        
-        //VUE
-            /**
-         * Generation de la vue 
-         */
-        
-            if($user == false){
-              $errorMsg = 'Profil introuvable';
-              $this->_view = new \CritsPortal\views\View('Error');
-              $this->_view->generate('Erreur', array('errorMsg' => $errorMsg));
-            } else{
-              $this->_view = new \CritsPortal\views\View('Profil');
-              $this->_view->generate('Crits-Profil', array('user' => $user));
-            }
-
-        }
-    
-  }
 }
-

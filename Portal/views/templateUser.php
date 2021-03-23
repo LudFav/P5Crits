2d40333efbf14e9792ffb37e9b00e3de7d5205b0
LudFav <45117462+LudFav@users.noreply.github.com>
2021-03-23 17:16:29 +0100

    Update templateUser.php

--- Portal/views/templateUser.php
+++ Portal/views/templateUser.php
@@ -16,16 +16,34 @@
 		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
-     <!-- Nav -->
-     <nav id="nav">
-            <ul class="links">
-                <li class="active"><a href="accueil">Accueil</a></li>
-                <li><a href="sommaire">Sommaire</a></li>
-                <li><a href="profil">Profil</a></li>
-                <li><a class="logout" id="logout">Déconnexion</a></li>
-                <!--li a >adresse a remplir vers dernier article php-->
-            </ul>
-        </nav>
+    <div id="wrapper" class="fade-in">
+
+<!-- Intro -->
+<div class="bg"></div>
+    <div id="intro">
+        <a href="accueil" class="logo"><img src='assets/images/Critstest.png' alt="logo"></a>
+        <p>Bienvenue sur le portail de Crits, une application en développement vous permettant de jouer en ligne a des jeux de rôle papier, pour le moment voyez ce portail tel un devblog.</p>
+        <ul class="actions">
+            <li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+        </ul>
+    </div>
+
+<!-- Header -->
+    <header id="header">
+        <a href="accueil" class="logo"><img src='assets/images/Critstest.png' alt="logo"></a>
+    </header>
+
+<!-- Nav -->
+
+ <nav id="nav">
+        <ul class="links">
+            <li class="active"><a href="accueil">Accueil</a></li>
+            <li><a href="sommaire">Sommaire</a></li>
+            <li><a href="profil">Profil</a></li>
+            <li><a href="" id="login" data-toggle="modal" data-target="#connexion">Connexion</a></li>
+        </ul>
+</nav>
+
     <!--Content-->
     <?= $content ?>	
     	<!-- Footer -->
@@ -51,6 +69,7 @@
                 <script src="assets/js/bootstrap.js"></script>
                 <script src="assets/js/Modal.js"></script>
                 <script src="assets/js/Pagination.js"></script>
+                <script src="assets/js/CharacterSheet.js"></script>
                 <script src="assets/js/ajaxUser.js"></script>
                 <script src="assets/js/ajax.js"></script>
                 <script src="assets/js/main.js"></script>

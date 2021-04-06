b3bc116095af6248745d20ef58c577e4cf93c7a9
LudFav <45117462+LudFav@users.noreply.github.com>
2021-04-06 15:37:34 +0200

    Update viewCharacter.php

--- Portal/views/viewCharacter.php
+++ Portal/views/viewCharacter.php
@@ -1,5 +1,28 @@
 <div class="row">
+ <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="pills-carac-tab" data-bs-toggle="pill" data-bs-target="#pills-carac" type="button" role="tab" aria-controls="pills-carac" aria-selected="true">Caractéristiques</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-ability-tab" data-bs-toggle="pill" data-bs-target="#pills-ability" type="button" role="tab" aria-controls="pills-ability" aria-selected="false">Abilités</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-skill-tab" data-bs-toggle="pill" data-bs-target="#pills-skill" type="button" role="tab" aria-controls="pills-skill" aria-selected="false">Compétences</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-gift-tab" data-bs-toggle="pill" data-bs-target="#pills-gift" type="button" role="tab" aria-controls="pills-gift" aria-selected="false">Dons</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-sorti-tab" data-bs-toggle="pill" data-bs-target="#pills-sorti" type="button" role="tab" aria-controls="pills-sorti" aria-selected="false">Sorts</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-invent-tab" data-bs-toggle="pill" data-bs-target="#pills-invent" type="button" role="tab" aria-controls="pills-invent" aria-selected="false">Inventaire</button>
+        </li>
+        <li class="nav-item" role="presentation">
+            <button class="nav-link" id="pills-fight-tab" data-bs-toggle="pill" data-bs-target="#pills-fight" type="button" role="tab" aria-controls="pills-fight" aria-selected="false">Combat</button>
+        </li>
+    </ul>
         <div class='container mt-5'>
-            <div id="tableCarac" class="row mx-n5"></div>
+            <div id="tableCarac" class="row tab-content mx-n5"></div>
         </div>
 </div>

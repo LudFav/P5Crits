256e61b02f2094bbf15a24f149da47c1735004b3
LudFav <45117462+LudFav@users.noreply.github.com>
2021-04-06 23:39:00 +0200

    Update templateUser.php

--- Portal/views/templateUser.php
+++ Portal/views/templateUser.php
@@ -45,18 +45,18 @@
 </nav>
 
     <!--Content-->
-    <?= $content ?>	
+    <?= $content ?>
     	<!-- Footer -->
         <footer id="footer">
-						
+
                         </footer>
                         <!-- Copyright -->
                         <div id="copyright">
                             <ul><li>&copy;CRITS - 2020</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
                         </div>
-    
+
                 </div>
-    
+
             <!-- Scripts -->
                 <script src="assets/js/jquery.min.js"></script>
                 <script src="assets/js/jquery.scrollex.min.js"></script>
@@ -64,9 +64,8 @@
                 <script src="assets/js/browser.min.js"></script>
                 <script src="assets/js/breakpoints.min.js"></script>
                 <script src="assets/js/util.js"></script>
-                <!--<script src="assets/js/jquery-3.4.1.min.js"></script>-->
-                <script src="assets/js/tether.min.js"></script>
                 <script src="assets/js/bootstrap.js"></script>
+                <script src="assets/js/tether.min.js"></script>
                 <script src="assets/js/Modal.js"></script>
                 <script src="assets/js/Pagination.js"></script>
                 <script src="assets/js/CharacterSheet.js"></script>

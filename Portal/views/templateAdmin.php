6d76043fe0e020ed78e102bda72cc83130776a31
LudFav <45117462+LudFav@users.noreply.github.com>
2021-04-06 23:39:02 +0200

    Update templateAdmin.php

--- Portal/views/templateAdmin.php
+++ Portal/views/templateAdmin.php
@@ -45,10 +45,10 @@
 </nav>
 
     <!--Content-->
-    <?= $content ?>	
+    <?= $content ?>
     	<!-- Footer -->
         <footer id="footer">
-						
+
                         </footer>
                         <div class="container">
                             <ul class="menu admin">
@@ -60,10 +60,10 @@
                         <div id="copyright">
                             <ul><li>&copy;CRITS - 2020</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li></ul>
                         </div>
-    
+
                 </div>
-    
-            <!-- Scripts -->            
+
+            <!-- Scripts -->
                 <script>window.onload = function() { document.body.classList.remove('is-preload'); }
                 	window.ontouchmove = function() { return false; }
                 	window.onorientationchange = function() { document.body.scrollTop = 0; }
@@ -91,7 +91,7 @@
                 <script src="assets/js/browser.min.js"></script>
                 <script src="assets/js/breakpoints.min.js"></script>
                 <script src="assets/js/util.js"></script>
-                <!--<script src="assets/js/jquery-3.4.1.min.js"></script>-->
+              
                 <script src="assets/js/tether.min.js"></script>
                 <script src="assets/js/bootstrap.js"></script>
                 <script src="assets/js/Modal.js"></script>

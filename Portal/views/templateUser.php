57b1a295351358200f7a1447dc61a288d28e4bd7
LudFav <45117462+LudFav@users.noreply.github.com>
2021-04-07 22:42:17 +0200

    Update templateUser.php

--- Portal/views/templateUser.php
+++ Portal/views/templateUser.php
@@ -65,6 +65,7 @@
                 <script src="assets/js/breakpoints.min.js"></script>
                 <script src="assets/js/util.js"></script>
                 <script src="assets/js/bootstrap.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
                 <script src="assets/js/tether.min.js"></script>
                 <script src="assets/js/Modal.js"></script>
                 <script src="assets/js/Pagination.js"></script>
